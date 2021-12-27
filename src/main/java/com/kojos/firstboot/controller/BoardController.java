0104be0bcda095f5869c8cc42d3bb5230cee3274
kojoscap <kojos@naver.com>
2021-12-27 17:16:14 +0900

    CRUD 완료 JPA Paging처리 작업중.

--- src/main/java/com/kojos/firstboot/controller/BoardController.java
+++ src/main/java/com/kojos/firstboot/controller/BoardController.java
@@ -2,12 +2,17 @@ package com.kojos.firstboot.controller;
 
 import com.kojos.firstboot.model.Board;
 import com.kojos.firstboot.repository.BoardRepository;
+import com.kojos.firstboot.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,10 +22,38 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
-    @GetMapping("list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+    @Autowired
+    private BoardValidator boardValidator;
+
+    @GetMapping("/list")
+    public String list(Model model, Pageable pageable){
+        Page<Board> boards = boardRepository.findAll(pageable);
+        int startPage = Math.max(0, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id==null){
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/kojos/firstboot/model/Board.java
+++ src/main/java/com/kojos/firstboot/model/Board.java
@@ -1,12 +1,12 @@
 package com.kojos.firstboot.model;
 
-
 import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -15,6 +15,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
+
+    @NotNull
+    @Size(min = 2, max = 30)
     private  String title;
     private  String content;
 }
